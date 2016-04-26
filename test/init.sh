dbbcdeab392a702aaa2fb67626e0b9a1b71a2d98
alvar-freude <github_ok@alvar.a-blast.org>
2016-04-26 19:04:35 +0200

    simple hack for starting a test cluster
    Added a simple shell script hack, which starts a new and empty
    PostgreSQL test cluster with pg_partman and pgtap.

---
+++ test/init.sh
@@ -0,0 +1,21 @@
+--
+-- Install and start a simple test cluster  
+--
+
+initdb -D /tmp/pgtest
+echo "shared_preload_libraries = 'pg_partman_bgw'" >/tmp/pgtest/postgresql.conf
+
+pg_ctl -D /tmp/pgtest start
+
+echo "Postgres started; wait for startup.";
+sleep 2
+
+psql -c 'CREATE SCHEMA partman; CREATE EXTENSION IF NOT EXISTS pg_partman SCHEMA partman; CREATE EXTENSION IF NOT EXISTS pgtap;' postgres
+
+
+echo "Postgres running (unless there are error mesages). Start testing with"
+echo 
+echo "  pg_prove -d postgres -ovf tests/*.sql"
+echo 
+echo "stop all with: pg_ctl -D /tmp/pgtest stop && rm -rvf /tmp/pgtest"
+
