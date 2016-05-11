b638fb6d219ec8dd99c19c4ab9557854d6ea776b
Piotr Przybylski <ppr@touk.pl>
2016-05-11 22:43:24 +0200

    Allow check_parent to work faster over large tables by skipping counting

--- sql/functions/check_parent.sql
+++ sql/functions/check_parent.sql
@@ -1,7 +1,7 @@
 /*
  * Function to monitor for data getting inserted into parent tables managed by extension
  */
-CREATE FUNCTION check_parent() RETURNS SETOF @extschema@.check_parent_table
+CREATE FUNCTION check_parent(p_exact_count boolean DEFAULT true) RETURNS SETOF @extschema@.check_parent_table
     LANGUAGE plpgsql STABLE SECURITY DEFINER
     AS $$
 DECLARE
@@ -24,7 +24,11 @@ LOOP
     WHERE schemaname = split_part(v_row.parent_table, '.', 1)::name
     AND tablename = split_part(v_row.parent_table, '.', 2)::name;
 
-    v_sql := format('SELECT count(1) AS n FROM ONLY %I.%I', v_schemaname, v_tablename);
+    IF p_exact_count THEN
+        v_sql := format('SELECT count(1) AS n FROM ONLY %I.%I', v_schemaname, v_tablename);
+    ELSE
+        v_sql := format('SELECT count(1) AS n FROM (SELECT * FROM ONLY %I.%I LIMIT 1) x', v_schemaname, v_tablename);
+    END IF;
 
     -- Each query creates an AccessShareLock on referenced table (along with all of its indexes)
     -- that PostgreSQL holds until parent transaction block COMMITs or ROLLBACKs, which can lead
