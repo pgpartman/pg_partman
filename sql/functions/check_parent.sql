aae9cda93a53a432398d10bbbd590bcb976cfbae
Piotr Przybylski <ppr@touk.pl>
2016-05-11 22:39:28 +0200

    Fix check_parent throwing 'out of shared memory error' when partman manages lots of tables

--- sql/functions/check_parent.sql
+++ sql/functions/check_parent.sql
@@ -16,7 +16,7 @@ v_trouble       @extschema@.check_parent_table%rowtype;
 BEGIN
 
 FOR v_row IN 
-    SELECT DISTINCT parent_table FROM @extschema@.part_config
+    SELECT parent_table FROM @extschema@.part_config
 LOOP
     SELECT schemaname, tablename 
     INTO v_schemaname, v_tablename
@@ -25,16 +25,25 @@ LOOP
     AND tablename = split_part(v_row.parent_table, '.', 2)::name;
 
     v_sql := format('SELECT count(1) AS n FROM ONLY %I.%I', v_schemaname, v_tablename);
-    EXECUTE v_sql INTO v_count;
+
+    -- Each query creates an AccessShareLock on referenced table (along with all of its indexes)
+    -- that PostgreSQL holds until parent transaction block COMMITs or ROLLBACKs, which can lead
+    -- to an 'out of shared memory' error. PL/pgSQL disallows SAVEPOINTs in PL/pgSQL,
+    -- but we can simulate ROLLBACK TO SAVEPOINT by throwing an exception within BEGIN...END block.
+    BEGIN
+        v_count := 0;
+        EXECUTE v_sql INTO v_count;
+        RAISE EXCEPTION USING errcode = 'PMRLB';
+    EXCEPTION
+        WHEN sqlstate 'PMRLB' THEN
+            NULL;
+    END;
 
     IF v_count > 0 THEN 
         v_trouble.parent_table := v_schemaname ||'.'|| v_tablename;
         v_trouble.count := v_count;
         RETURN NEXT v_trouble;
     END IF;
-
-    v_count := 0;
-
 END LOOP;
 
 RETURN;
