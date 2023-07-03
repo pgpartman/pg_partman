4e32eba349d9fac3da594d13861a460c7cab40e1
Vitaly Burovoy <vitaly.burovoy@gmail.com>
2023-07-03 20:00:09 +0000

    Use sql/tables/*.sql for an update script [1/5]
    Split table creation and copying data.
    
    The best way to watch changes is:
    git diff --color-moved HEAD^..HEAD

--- updates/pg_partman--4.7.3--5.0.0-beta.sql
+++ updates/pg_partman--4.7.3--5.0.0-beta.sql
@@ -123,7 +123,6 @@ SELECT
     , sub_ignore_default_data
 FROM @extschema@.part_config_sub;
 
-DROP TABLE @extschema@.part_config_sub;
 
 INSERT INTO @extschema@.part_config_pre_500_data (
     parent_table
@@ -190,6 +189,7 @@ SELECT
     , ignore_default_data
 FROM @extschema@.part_config;
 
+DROP TABLE @extschema@.part_config_sub;
 DROP TABLE @extschema@.part_config;
 
 CREATE TABLE @extschema@.part_config (
@@ -224,6 +224,62 @@ CREATE TABLE @extschema@.part_config (
 
 CREATE INDEX part_config_type_idx ON @extschema@.part_config (partition_type);
 SELECT pg_catalog.pg_extension_config_dump('@extschema@.part_config'::regclass, '');
+
+CREATE TABLE @extschema@.part_config_sub (
+    sub_parent text
+    , sub_control text NOT NULL
+    , sub_partition_interval text NOT NULL
+    , sub_partition_type text NOT NULL
+    , sub_premake int NOT NULL DEFAULT 4
+    , sub_automatic_maintenance text NOT NULL DEFAULT 'on'
+    , sub_template_table text
+    , sub_retention text
+    , sub_retention_schema text
+    , sub_retention_keep_index boolean NOT NULL DEFAULT true
+    , sub_retention_keep_table boolean NOT NULL DEFAULT true
+    , sub_epoch text NOT NULL DEFAULT 'none'
+    , sub_constraint_cols text[]
+    , sub_optimize_constraint int NOT NULL DEFAULT 30
+    , sub_infinite_time_partitions boolean NOT NULL DEFAULT false
+    , sub_jobmon boolean NOT NULL DEFAULT true
+    , sub_inherit_privileges boolean DEFAULT false
+    , sub_constraint_valid boolean DEFAULT true NOT NULL
+    , sub_subscription_refresh text
+    , sub_ignore_default_data boolean NOT NULL DEFAULT true
+    , sub_default_table boolean default true
+    , sub_date_trunc_interval TEXT
+    , CONSTRAINT part_config_sub_pkey PRIMARY KEY (sub_parent)
+    , CONSTRAINT part_config_sub_sub_parent_fkey FOREIGN KEY (sub_parent) REFERENCES @extschema@.part_config (parent_table) ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED
+    , CONSTRAINT positive_premake_check CHECK (sub_premake > 0)
+);
+SELECT pg_catalog.pg_extension_config_dump('@extschema@.part_config_sub'::regclass, '');
+
+ALTER TABLE @extschema@.part_config ADD CONSTRAINT control_constraint_col_chk CHECK ((constraint_cols @> ARRAY[control]) <> true);
+ALTER TABLE @extschema@.part_config_sub ADD CONSTRAINT control_constraint_col_chk CHECK ((sub_constraint_cols @> ARRAY[sub_control]) <> true);
+
+ALTER TABLE @extschema@.part_config ADD CONSTRAINT retention_schema_not_empty_chk CHECK (retention_schema <> '');
+ALTER TABLE @extschema@.part_config_sub ADD CONSTRAINT retention_schema_not_empty_chk CHECK (sub_retention_schema <> '');
+
+ALTER TABLE @extschema@.part_config
+ADD CONSTRAINT part_config_automatic_maintenance_check
+CHECK (@extschema@.check_automatic_maintenance_value(automatic_maintenance));
+
+ALTER TABLE @extschema@.part_config_sub
+ADD CONSTRAINT part_config_sub_automatic_maintenance_check
+CHECK (@extschema@.check_automatic_maintenance_value(sub_automatic_maintenance));
+
+ALTER TABLE @extschema@.part_config
+ADD CONSTRAINT part_config_epoch_check
+CHECK (@extschema@.check_epoch_type(epoch));
+
+ALTER TABLE @extschema@.part_config_sub
+ADD CONSTRAINT part_config_sub_epoch_check
+CHECK (@extschema@.check_epoch_type(sub_epoch));
+
+-- TEMPORARY; it is safe because part_config*_pre_500_data have no constraints
+-- and exists only inside the CREATE EXTENSION transaction.
+UPDATE @extschema@.part_config_pre_500_data SET partition_type = 'range' WHERE partition_type = 'native';
+UPDATE @extschema@.part_config_sub_pre_500_data SET sub_partition_type = 'range' WHERE sub_partition_type = 'native';
 
 INSERT INTO @extschema@.part_config (
     parent_table
@@ -277,35 +333,6 @@ SELECT
 FROM @extschema@.part_config_pre_500_data;
 
 
-CREATE TABLE @extschema@.part_config_sub (
-    sub_parent text
-    , sub_control text NOT NULL
-    , sub_partition_interval text NOT NULL
-    , sub_partition_type text NOT NULL
-    , sub_premake int NOT NULL DEFAULT 4
-    , sub_automatic_maintenance text NOT NULL DEFAULT 'on'
-    , sub_template_table text
-    , sub_retention text
-    , sub_retention_schema text
-    , sub_retention_keep_index boolean NOT NULL DEFAULT true
-    , sub_retention_keep_table boolean NOT NULL DEFAULT true
-    , sub_epoch text NOT NULL DEFAULT 'none'
-    , sub_constraint_cols text[]
-    , sub_optimize_constraint int NOT NULL DEFAULT 30
-    , sub_infinite_time_partitions boolean NOT NULL DEFAULT false
-    , sub_jobmon boolean NOT NULL DEFAULT true
-    , sub_inherit_privileges boolean DEFAULT false
-    , sub_constraint_valid boolean DEFAULT true NOT NULL
-    , sub_subscription_refresh text
-    , sub_ignore_default_data boolean NOT NULL DEFAULT true
-    , sub_default_table boolean default true
-    , sub_date_trunc_interval TEXT
-    , CONSTRAINT part_config_sub_pkey PRIMARY KEY (sub_parent)
-    , CONSTRAINT part_config_sub_sub_parent_fkey FOREIGN KEY (sub_parent) REFERENCES @extschema@.part_config (parent_table) ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED
-    , CONSTRAINT positive_premake_check CHECK (sub_premake > 0)
-);
-SELECT pg_catalog.pg_extension_config_dump('@extschema@.part_config_sub'::regclass, '');
-
 INSERT INTO @extschema@.part_config_sub (
     sub_parent
     , sub_control
@@ -352,32 +379,6 @@ SELECT
     , sub_date_trunc_interval
     , sub_ignore_default_data
 FROM @extschema@.part_config_sub_pre_500_data;
-
-
-ALTER TABLE @extschema@.part_config ADD CONSTRAINT control_constraint_col_chk CHECK ((constraint_cols @> ARRAY[control]) <> true);
-ALTER TABLE @extschema@.part_config_sub ADD CONSTRAINT control_constraint_col_chk CHECK ((sub_constraint_cols @> ARRAY[sub_control]) <> true);
-
-ALTER TABLE @extschema@.part_config ADD CONSTRAINT retention_schema_not_empty_chk CHECK (retention_schema <> '');
-ALTER TABLE @extschema@.part_config_sub ADD CONSTRAINT retention_schema_not_empty_chk CHECK (sub_retention_schema <> '');
-
-ALTER TABLE @extschema@.part_config
-ADD CONSTRAINT part_config_automatic_maintenance_check
-CHECK (@extschema@.check_automatic_maintenance_value(automatic_maintenance));
-
-ALTER TABLE @extschema@.part_config_sub
-ADD CONSTRAINT part_config_sub_automatic_maintenance_check
-CHECK (@extschema@.check_automatic_maintenance_value(sub_automatic_maintenance));
-
-ALTER TABLE @extschema@.part_config
-ADD CONSTRAINT part_config_epoch_check
-CHECK (@extschema@.check_epoch_type(epoch));
-
-ALTER TABLE @extschema@.part_config_sub
-ADD CONSTRAINT part_config_sub_epoch_check
-CHECK (@extschema@.check_epoch_type(sub_epoch));
-
-UPDATE @extschema@.part_config SET partition_type = 'range' WHERE partition_type = 'native';
-UPDATE @extschema@.part_config_sub SET sub_partition_type = 'range' WHERE sub_partition_type = 'native';
 
 -- Allow list/hash in future update
 CREATE OR REPLACE FUNCTION @extschema@.check_partition_type (p_type text) RETURNS boolean
