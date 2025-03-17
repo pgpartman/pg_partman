3218c1f7737b7acc74835246f304a8a6c8f3c010
mrusmanshahid <usman-345@hotmail.com>
2025-03-17 17:05:10 +0100

    fix: template inheritance to optional to avoid breaking the create parent call

--- sql/functions/create_parent.sql
+++ sql/functions/create_parent.sql
@@ -635,8 +635,9 @@ IF p_default_table THEN
     PERFORM @extschema@.inherit_replica_identity(v_parent_schema, v_parent_tablename, v_default_partition);
 
     -- Manage template inherited properties
-    PERFORM @extschema@.inherit_template_properties(p_parent_table, v_parent_schema, v_default_partition);
-
+    IF v_template_tablename IS NOT NULL THEN
+        PERFORM @extschema@.inherit_template_properties(p_parent_table, v_parent_schema, v_partition_name);
+    END IF;
 END IF;
 
 
