e98ae9577811702ee17b2b8d3a5fe00d811eec5e
Jens Wilke <jens.wilke1@1und1.de>
2025-08-14 15:19:19 +0200

    fix deadlocks with run_maintenance #796

--- sql/functions/run_maintenance.sql
+++ sql/functions/run_maintenance.sql
@@ -225,6 +225,9 @@ LOOP
 
         -- Loop through child tables starting from highest to get a timestamp from the highest non-empty partition in the set
         -- Avoids doing a scan on entire partition set and/or getting any values accidentally in default.
+        -- Lock the parent_table first to prevent deadlocks
+        RAISE DEBUG 'PERFORM LOCK %.%', v_parent_schema, v_parent_tablename;
+        PERFORM format('LOCK ''%I.%I''::regclass', v_parent_schema, v_parent_tablename);
         FOR v_row_max_time IN
             SELECT partition_schemaname, partition_tablename FROM @extschema@.show_partitions(v_row.parent_table, 'DESC', false)
         LOOP
