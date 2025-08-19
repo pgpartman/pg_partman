85fd79077ca73a9de43827722d7032c079ff9cd9
Jens Wilke <jens.wilke1@1und1.de>
2025-08-19 12:13:29 +0200

    warn about version mismatch #735

--- sql/functions/run_maintenance.sql
+++ sql/functions/run_maintenance.sql
@@ -98,7 +98,7 @@ EXECUTE format('SELECT set_config(%L, %L, %L)', 'search_path', v_new_search_path
 
 SELECT true INTO v_version_mismatch FROM pg_available_extensions WHERE name = 'pg_partman' AND default_version != installed_version;
 IF v_version_mismatch THEN
-    RAISE WARNING 'A new version of pg_partman is available. run_maintenance might nor work as expected. Run ALTER EXTENSION pg_partman UPDATE first.';
+    RAISE WARNING 'A new version of pg_partman is available. run_maintenance might not work as expected. Run ALTER EXTENSION pg_partman UPDATE first.';
 END IF;
 
 IF v_jobmon_schema IS NOT NULL THEN
