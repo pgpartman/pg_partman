7f90cf3995a3e14f2be9a57f1586d9e00fbaf5a4
Jens Wilke <jens.wilke1@1und1.de>
2025-08-19 10:04:01 +0200

    an exclusive lock is required

--- sql/functions/run_maintenance.sql
+++ sql/functions/run_maintenance.sql
@@ -358,7 +358,7 @@ LOOP
         v_current_partition_id := NULL;
 
         -- Lock the parent_table first to prevent deadlocks
-        RAISE DEBUG 'PERFORM LOCK %.% IN access share mode', v_parent_schema, v_parent_tablename;
+        RAISE DEBUG 'PERFORM LOCK %.%', v_parent_schema, v_parent_tablename;
         PERFORM format('LOCK ''%I.%I''::regclass', v_parent_schema, v_parent_tablename);
         FOR v_row_max_id IN
             SELECT partition_schemaname, partition_tablename FROM @extschema@.show_partitions(v_row.parent_table, 'DESC', false)
