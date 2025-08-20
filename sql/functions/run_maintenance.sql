10eb7ea7a26ad6b3fcaab0de950c55dcd2e0399d
jw1u1 <89261565+jw1u1@users.noreply.github.com>
2025-08-20 11:13:31 +0200

    Typo

--- sql/functions/run_maintenance.sql
+++ sql/functions/run_maintenance.sql
@@ -97,9 +97,9 @@ IF p_jobmon THEN
 END IF;
 EXECUTE format('SELECT set_config(%L, %L, %L)', 'search_path', v_new_search_path, 'false');
 
-SELECT default_version, installed_version INTO v_default_version, v_intalled_version FROM pg_available_extensions WHERE name = 'pg_partman' AND default_version != installed_version;
+SELECT default_version, installed_version INTO v_default_version, v_installed_version FROM pg_available_extensions WHERE name = 'pg_partman' AND default_version != installed_version;
 IF v_intalled_version IS NOT NULL THEN
-    RAISE WARNING 'pg_partman version % is installed but version % is default. run_maintenance might not work as expected. A restart might be required after update.', v_intalled_version, v_default_version;
+    RAISE WARNING 'pg_partman version % is installed but version % is default. run_maintenance might not work as expected. A restart might be required after update.', v_installed_version, v_default_version;
 END IF;
 
 IF v_jobmon_schema IS NOT NULL THEN
